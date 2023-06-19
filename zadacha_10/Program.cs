b021b0d5241206df3e5a108b9d37014cefc5ba2d
Pecherskiy <applemors@mail.ru>
2023-06-19 20:06:42 +0300

    first commit

---
+++ zadacha_10/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+Random rand = new Random();
+int number = new Random().Next (100,1000);
+int firstNum = number / 100;
+int secNum = number % 10;
+int thirdNum = number / 10;
+int result = ((number - (firstNum*100)) / 10);
+Console.Write($"{number} - Полученное число ;");
+Console.Write($" {result} - Выделенная цифра");
+
---
+++ zadacha_15/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да 8989
+1 -> нет
+*/
+
+int Number = ReadInt("Введите число от 1 до 7: ");
+Console.WriteLine(Day(Number));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+string Day(int a)
+{
+    if (a > 0 && a < 8)
+    {
+        if (a == 7 || a == 6)
+        {
+            Console.Write( a + " - Выходной");
+        }
+        else
+        {
+            Console.Write(a + " - Рабочий");
+        }
+    }
+    else
+    {
+        Console.Write("Неверно выбран");
+    }
+    return " день.";
+}
